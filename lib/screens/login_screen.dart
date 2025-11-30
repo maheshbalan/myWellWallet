2ec0b272f01cf1be601c40217bcbe5af4605a945
maheshbalan <mahesh.balan@gmail.com>
2025-11-30 12:45:35 -0700

    fix: Make login screen scrollable to prevent overflow in landscape and fix mcp_client_sse parsing

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -106,11 +106,17 @@ class _LoginScreenState extends State<LoginScreen> {
 
     return Scaffold(
       body: SafeArea(
-        child: Padding(
+        child: SingleChildScrollView(
           padding: const EdgeInsets.all(24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: MediaQuery.of(context).size.height - 
+                         MediaQuery.of(context).padding.top - 
+                         MediaQuery.of(context).padding.bottom - 48,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
               // App Icon
               Container(
                 width: 120,
@@ -239,7 +245,8 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                 ),
               ],
-            ],
+              ],
+            ),
           ),
         ),
       ),
